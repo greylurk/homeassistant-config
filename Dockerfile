5ccb202e2251f09d7246d44b43f8c889ce41451f
Adam Ness <adam.ness@gmail.com>
2017-08-04 08:18:38 -0500

    Removing docker stuff, migrating to hassio

--- Dockerfile
+++
@@ -1,5 +0,0 @@
-FROM homeassistant/home-assistant
-
-RUN apt-get update && apt-get install ffmpeg
-
-
--- start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -d --name="home-assistant" -v /etc/letsencrypt:/etc/letsencrypt -v /home/adamn/hass:/config -v /etc/localtime:/etc/localtime:ro --net=host greylurk/home-assistant
